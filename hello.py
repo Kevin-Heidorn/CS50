7aeb9f17a7cb962d75fe2127e8badbee54c37385
Kevin-Heidorn <kevinheidorn20@gmail.com>
2024-12-06 16:11:17 -0300

    first hour in CS50

---
+++ hello.py
@@ -0,0 +1,8 @@
+#Functions and Variables in Python
+ 
+#Ask for name 
+name = input("What's your name? ").strip().title()
+
+#print (' Hello, {}'.format(name))
+#Can also use 
+print (f'Hello, {name}')
