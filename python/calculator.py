3db26a3eb3b0100c416efcb8b855868f39db61ce
Kevin-Heidorn <kevinheidorn20@gmail.com>
2024-12-08 12:12:03 -0300

    att

---
+++ python/calculator.py
@@ -0,0 +1,22 @@
+#int 
+
+x = int(input("First Number: "))
+y = int(input("Second Number: "))
+print (x + y)
+
+#float 
+
+x = float(input("First Number: "))
+y = float(input("Second Number: "))
+
+z = (round(x+y)) #Round = Number around 
+print(F"{z:,}") # formatting :,
+
+
+#Division 
+x = float(input("First Number: "))
+y = float(input("Second Number: "))
+
+z = (x/y)
+print(f"{z:.2f}")
+
--- python/hello.py
+++ python/hello.py
@@ -3,6 +3,11 @@
 #Ask for name 
 name = input("What's your name? ").strip().title()
 
+
+
+#split user name 
+#first, last = name.split(" ")
+
 #print (' Hello, {}'.format(name))
 #Can also use 
 print (f'Hello, {name}')
